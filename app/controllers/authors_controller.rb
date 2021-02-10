e626e2d52c8e300a8d7e6a840469ae7309be2f63
github username <scott.gloyna01@gmail.com>
2021-02-09 22:35:11 -0600

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,6 +1,7 @@
 class AuthorsController < ApplicationController
+  before_action :set_author, only: [:show, :edit, :update]
+
   def show
-    @author = Author.find(params[:id])
   end
 
   def new
@@ -8,14 +9,53 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create(author_params)
+    @author = Author.new(author_params)
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
+  end
 
-    redirect_to author_path(@author)
+  def edit
+  end
+
+  def update
+    if @author.update(author_params)
+      redirect_to author_path(@author)
+    else
+      render :edit
+    end
   end
 
   private
 
   def author_params
-    params.permit(:name, :email, :phone_number)
+    params.permit(:email, :name, :phone_number)
   end
+
+  def set_author
+    @author = Author.find(params[:id])
+  end
+
+  # <%= form_tag authors_path, method: "post" do %>
+  #   <div class="field">
+  #     <%= label_tag "name", "Name" %>
+  #     <%= text_field_tag "name" %>
+  #   </div>
+  
+  #   <div class="field">
+  #     <%= label_tag "email", "Email" %>
+  #     <%= text_field_tag "email" %>
+  #   </div>
+  
+  #   <div class="field">
+  #     <%= label_tag "phone_number", "Phone Number" %>
+  #     <%= text_field_tag "phone_number" %>
+  #   </div>
+  
+  #   <%= submit_tag "Create" %>
+  # <% end %>
+
 end
